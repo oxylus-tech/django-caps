b496ce07b3a9b2870113a3c81adb3fa82d450ffc
bkfox <thomas bkfox net>
2025-05-29 15:40:01 +0200

    add missing import

--- caps/models/__init__.py
+++ caps/models/__init__.py
@@ -1,11 +1,12 @@
 from .agent import Agent, AgentQuerySet
-from .object import Object
+from .object import Object, ObjectQuerySet
 from .access import Access, AccessQuerySet
 
 __all__ = (
     "Agent",
     "AgentQuerySet",
     "Object",
+    "ObjectQuerySet",
     "Access",
     "AccessQuerySet",
 )
