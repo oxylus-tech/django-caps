23945f4f0636ad6a4a0f0d9c31227084a9c9a933
bkfox <thomas bkfox net>
2025-06-03 19:28:37 +0200

    upgrade migrations

---
+++ caps/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import User, Group
+
+from .models import Agent
+
+
+@receiver(post_save, sender=User)
+def create_user_agent(sender, instance, created, *args, **kwargs):
+    if not hasattr(instance, "agent"):
+        Agent.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Group)
+def create_group_agent(sender, instance, created, *args, **kwargs):
+    if not hasattr(instance, "agent"):
+        Agent.objects.create(group=instance)
